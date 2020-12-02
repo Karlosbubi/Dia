f9150524ac1c578eabf82a27ea5ffb3095c725ec
Kurt Brunner <Kurt@Karlosbubi.de>
2020-12-02 22:12:44 +0100

    init

---
+++ task1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <list>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main () {
+  string line;
+  ifstream myfile ("task1-input.txt");
+  if (myfile.is_open())
+  {
+    while ( getline (myfile,line) )
+    {
+      cout << line << '\n';
+    }
+    myfile.close();
+  }
+
+  else cout << "Unable to open file"; 
+
+  return 0;
+}
