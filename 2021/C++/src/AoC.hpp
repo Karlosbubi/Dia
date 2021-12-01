ebbe74f1be2520573833a44984cb4a4c41477ce1
Kurt Brunner <Kontakt@Karlosbubi.de>
2021-12-01 10:30:55 +0100

    annoiying templates

---
+++ 2021/C++/src/AoC.hpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+template <typename I, typename O>
+struct test_input
+{
+    I input;
+    O output;
+    test_input(I in, O out)
+    {
+        input = in;
+        output = out;
+    }
+};
+
+class AoC
+{
+    template <typename T>
+    T readInput(std::string path);
+    template <typename T>
+    auto task1(T input);
+    template <typename T>
+    auto task2(T input);
+
+public:
+    template <typename T>
+    static int run(const std::string path)
+    {
+        std::cout << "Task 1 result : " << task1(readInput(path)) << "\n";
+        std::cout << "Task 2 result : " << task2(readInput(path)) << "\n";
+
+        return 0;
+    }
+
+    template <typename I, typename O>
+    static bool test(std::vector<test_input<I, O>> t_task1, std::vector<test_input<I, O>> t_task2)
+    {
+        std::cout << "Testing : \n";
+        int i = 0;
+        for (auto t1 : t_task1)
+        {
+            i++;
+            if (task1<I>(t1.input) != t1.output)
+            {
+                std::cout << "Test Nr. " << i << " von Task 1 failed\n";
+                return false;
+            }
+            std::cout << "Test Nr. " << i << " von Task 1 passed\n";
+        }
+
+        i = 0;
+        for (auto t2 : t_task2)
+        {
+            i++;
+            if (task1(t2<I>.input) != t2.output)
+            {
+                std::cout << "Test Nr. " << i << " von Task 2 failed\n";
+                return false;
+            }
+            std::cout << "Test Nr. " << i << " von Task 2 passed\n";
+        }
+
+        std::cout << "\nAll Tests Passed !\n\n";
+        return true;
+    }
+};
--- 2021/C++/src/day1.cpp
+++ 2021/C++/src/day1.cpp
@@ -1,9 +1,41 @@
-#include <iostream>
+#include "AoC.hpp"
 
 using namespace std;
 
+vector<test_input<vector<int>, int>> t_t1;
+t_t1.push_back(test_input({199, 200, 208, 210, 200, 207, 240, 269, 260, 263}, 7));
+
 int main()
 {
-    cout << "Hello World !\n";
-    return 0;
+    cout << "Hello Day 1\n";
+    return AoC::test<vector<int>, int>(t_t1, ) ? -1 : AoC::run<vector<int>>(string("../input/day1.txt"));
+}
+
+vector<int> readInput(string path)
+{
+}
+
+int task1(vector<int> input)
+{
+}
+
+bool tests()
+{
+    //task 1
+    vector<int> input = {199, 200, 208, 210, 200, 207, 240, 269, 260, 263};
+
+    int output = 7;
+
+    if (output != task1(input))
+    {
+        cout << "Day 1 task 1 failed. \n";
+        cout << "Expected : " << output << " got : " << task1(input) << ".\n";
+        return false;
+    }
+    else
+    {
+        cout << "Day 1 task 1 passed. \n";
+    }
+
+    return true;
 }
