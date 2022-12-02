4d5b32d0bded7041ea72027195ee656ef21a01e6
Kurt Brunner <Kontakt@Karlosbubi.de>
2022-12-02 16:15:34 +0100

    2022

---
+++ 2022/Rust/day1/target/debug/deps/day1-4398c410b3cf7d0f.d
@@ -0,0 +1,5 @@
+/home/kurt/Projects/Dia/2022/Rust/day1/target/debug/deps/day1-4398c410b3cf7d0f.rmeta: src/main.rs
+
+/home/kurt/Projects/Dia/2022/Rust/day1/target/debug/deps/day1-4398c410b3cf7d0f.d: src/main.rs
+
+src/main.rs:
---
+++ 2022/Rust/day1/target/debug/deps/day1-d9e3ded6e8ae464a.d
@@ -0,0 +1,5 @@
+/home/kurt/Projects/Dia/2022/Rust/day1/target/debug/deps/day1-d9e3ded6e8ae464a: src/main.rs
+
+/home/kurt/Projects/Dia/2022/Rust/day1/target/debug/deps/day1-d9e3ded6e8ae464a.d: src/main.rs
+
+src/main.rs:
