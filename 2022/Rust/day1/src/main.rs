c849540ec787f2440b2b990ec05866814f425f29
Kurt Brunner <Kontakt@Karlosbubi.de>
2022-12-02 17:02:33 +0100

    day1

--- 2022/Rust/day1/src/main.rs
+++ 2022/Rust/day1/src/main.rs
@@ -1,3 +1,43 @@
+use std::thread::Result;
+
 fn main() {
     println!("Hello, world!");
+
+    let input = read_input(String::from("./input.txt"));
+    println!("{}", task1(input.clone()));
+    println!("{}", task2(input));
+}
+
+fn read_input(path: String) -> Vec<Vec<i32>> {
+    let file = std::fs::read_to_string(path).expect("ERROR: Reading File\n");
+    let elves: Vec<&str> = file.split('\n').collect();
+
+    let mut result = Vec::new();
+    let mut tmp = Vec::new();
+
+    for elf in elves {
+        if elf != "" {
+            let i: i32 = elf.parse().expect("ERROR: Parsing File");
+            tmp.push(i);
+        } else {
+            result.push(tmp);
+            tmp = Vec::new();
+        }
+    }
+
+    return result;
+}
+
+fn task1(input: Vec<Vec<i32>>) -> i32 {
+    let mut summed: Vec<i32> = input.iter().map(|e| e.iter().sum()).collect();
+    summed.sort();
+
+    summed.pop().expect("missing value")
+}
+
+fn task2(input: Vec<Vec<i32>>) -> i32 {
+    let mut summed: Vec<i32> = input.iter().map(|e| e.iter().sum()).collect();
+    summed.sort();
+
+    summed.drain((summed.len() - 3)..).sum()
 }
---
+++ 2022/Rust/day1/target/debug/day1.d
@@ -0,0 +1,1 @@
+/home/kurt/Projects/Dia/2022/Rust/day1/target/debug/day1: /home/kurt/Projects/Dia/2022/Rust/day1/src/main.rs
