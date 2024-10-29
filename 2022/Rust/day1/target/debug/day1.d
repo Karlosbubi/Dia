d606c42323816e1d4dc5cdacc2ea69d38dc0a278
Kurt B. <Kontakt@Karlosbubi.de>
2024-10-29 10:00:19 +0100

    Delete 2022/Rust/day1/target directory

--- 2022/Rust/day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/kurt/Projects/Dia/2022/Rust/day1/target/debug/day1: /home/kurt/Projects/Dia/2022/Rust/day1/src/main.rs
--- 2022/Rust/day1/target/debug/deps/day1-63ccfe024879e78d.d
+++
@@ -1,5 +0,0 @@
-/home/kurt/Projects/Dia/2022/Rust/day1/target/debug/deps/day1-63ccfe024879e78d.rmeta: src/main.rs
-
-/home/kurt/Projects/Dia/2022/Rust/day1/target/debug/deps/day1-63ccfe024879e78d.d: src/main.rs
-
-src/main.rs:
