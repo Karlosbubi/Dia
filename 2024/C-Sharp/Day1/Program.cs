67408f7149e0733b0c6ed6d4602a778f50514fd3
Kurt Brunner <Kurt@Brunner.codes>
2024-12-01 16:19:10 +0100

    2024 Day1 C#

---
+++ 2024/C-Sharp/Day1/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Utils;
+
+Console.WriteLine("Hello, World!");
+
+var input = LoadInput.AsLines("Day1");
+
+var list1 = new List<int>();
+var list2 = new List<int>();
+
+input.ToList().ForEach(line=>
+{
+    var parts = line.Split(' ');
+    if (parts.Length >= 2)
+    {
+        list1.Add(int.Parse(parts.First()));
+        list2.Add(int.Parse(parts.Last()));
+    }
+});
+
+list1.Sort();
+list2.Sort();
+
+var diff = list1.Zip(list2, (x, y) =>
+{
+    if (x > y)
+    {
+        return x - y;
+    }
+    else
+    {
+        return y - x;
+    }
+}).ToArray();
+
+Console.WriteLine($"Day 1 Part 1: {diff.Sum()}");
+
+var similarity = list1.Select(x => x * list2.Count(y => y == x));
+Console.WriteLine($"Day 1 Part 2: {similarity.Sum()}");
---
+++ 2024/C-Sharp/Utils/LoadInput.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+namespace Utils;
+
+public static class LoadInput
+{
+    public static string InputDirectory { get; } = Environment.GetFolderPath(System.Environment.SpecialFolder.UserProfile) + "/Projects/Dia/Inputs/2024";
+
+    public static IEnumerable<string> AsLines(string day)
+    {
+        var path = InputDirectory + "\\" + day + ".txt";
+        return File.ReadLines(path);
+    } 
+}
